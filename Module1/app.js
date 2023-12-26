3b1ec6303e5b73b8f1e9a0e9ca7d57ad46ac92e5
NguyenHien <ngnghien5588@gmail.com>
2023-12-26 20:00:39 +0700

    ex-1

--- Module1/app.js
+++ Module1/app.js
@@ -1,1 +1,31 @@
-console.log("hello");
+//Bai1
+const printGreeting = () => {
+  return "Hello World!";
+};
+
+//Bai2
+const declareVariable = () => {
+  let firstVar = "Hello World !";
+  return firstVar;
+};
+
+//Bai3
+
+const printExpression = () => {
+  let firstVar = "Hello";
+  let secondVar = "World!";
+  let result = `${firstVar} ${secondVar}`;
+  return result;
+};
+
+//Bai4
+
+const main = () => {
+  const exercise_1 = printGreeting();
+  const exercise_2 = declareVariable();
+  const exercise_3 = printExpression();
+  console.log("Bai1: ", exercise_1);
+  console.log("Bai2: ", exercise_2);
+  console.log("Bai3: ", exercise_3);
+};
+main();
